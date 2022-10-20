7e5f1334eb612b8752d9cb77ab60631d2211013e
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2022-10-21 00:42:17 +0100

    Implemented footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillMail, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>@ 2022 1uc.as</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href="https://github.com/lucas-ps/">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/lucaspsmith/">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="malito:hi@1uc.as">
+        <AiFillMail size="3rem"/>
+      </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
